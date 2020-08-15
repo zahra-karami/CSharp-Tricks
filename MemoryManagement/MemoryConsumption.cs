9bec741d1db8420f968b25cda3dfe6e8eaa4a084
zahra-karami <zahra.karami@live.com>
2020-08-15 14:56:01 -0400

    Get memory usage in C#

---
+++ MemoryManagement/MemoryConsumption.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Diagnostics;
+
+
+namespace CsharpTricks.MemoryManagement
+{
+    public class MemoryConsumption
+    {
+        public void GetMemoryUsage()
+        {
+            var procName = System.Diagnostics.Process.GetCurrentProcess().ProcessName;
+
+            // dotnet add package System.Diagnostics.PerformanceCounter 
+            using PerformanceCounter pc = new PerformanceCounter("Process", "Private Bytes", procName);
+            Console.WriteLine(pc.NextValue());
+        }
+    }
+}
